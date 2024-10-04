6a287267e02b19cc5f52df9ed242189523134af0
Daniel Popchenko <104201822+DanielPopchenko@users.noreply.github.com>
2024-10-04 11:52:30 -0400

    Expo init project setup

---
+++ assets/data/products.ts
@@ -0,0 +1,30 @@
+export const products = [
+	{
+		id: 1,
+		name: 'Pepperoni Pizza',
+		imageUri:
+			'https://www.simplyrecipes.com/thmb/KE6iMblr3R2Db6oE8HdyVsFSj2A=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__09__easy-pepperoni-pizza-lead-3-1024x682-583b275444104ef189d693a64df625da.jpg',
+		price: 12.99,
+	},
+	{
+		id: 1,
+		name: '4 Cheese Pizza',
+		imageUri:
+			'https://www.insidetherustickitchen.com/wp-content/uploads/2020/07/Quattro-formaggi-pizza-square-Inside-the-rustic-kitchen.jpg',
+		price: 14.99,
+	},
+	{
+		id: 1,
+		name: 'Pizza',
+		imageUri:
+			'https://www.allrecipes.com/thmb/0xH8n2D4cC97t7mcC7eT2SDZ0aE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/6776_Pizza-Dough_ddmfs_2x1_1725-fdaa76496da045b3bdaadcec6d4c5398.jpg',
+		price: 15.99,
+	},
+	{
+		id: 1,
+		name: 'Vegan Pizza',
+		imageUri:
+			'https://thebananadiaries.com/wp-content/uploads/2023/01/vegan-pizza-dough_5658.jpg',
+		price: 14.99,
+	},
+];
